73ea21ee363c53bfaacbb0aa9827cf7f8678722b
Source-Kod <Source-Kod@protonmail.com>
2023-03-23 00:21:43 -0500

    1-7 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, int) {
 
-};
+  if(int<0) {
+    return 'ERROR';
+  }
+
+  return str.repeat(int);
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let total = 0;
 
+  if((num1 < 0 || num2 < 0) || !(typeof num1 === 'number' && typeof num2 === 'number')) {
+    return 'ERROR';
+  }
+  
+  if(num1 > num2) {
+    [num1,num2] = [num2,num1];
+  }
+
+  for(i=num1; i <= num2; i++) {
+    total = total + i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
